3fe5a8d825f654072d9703a265510d7cbc823389
Ueong2 <kiko130@naver.com>
2025-03-10 17:21:09 +0900

    좋아요 구현2 및 이미지, css 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <h1>영화정보</h1>
-  <div v-for="(movie, i) in data" :key="i">
-    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
-    <p>개봉: {{ movie.year }}</p>
-    <p>장르: {{ movie.category }}</p>
-    <!-- @ 대신 v-on 을 사용해도 됨-->
-    <button @:click="like++">좋아요</button><span> {{ like }}</span>
+  <div v-for="(movie, i) in data" :key="i" class="item">
+    <figure>
+      <img :src="`${movie.imgURL}`" :alt="movie.title">
+    </figure>
+    <div class="info">
+      <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+      <p>개봉: {{ movie.year }}</p>
+      <p>장르: {{ movie.category }}</p>
+      <!-- @ 대신 v-on 을 사용해도 됨-->
+      <button @:click="increaseLike(i)">좋아요</button>
+      <span> {{ movie.like }}</span>
+    </div>
   </div>
 </template>
 
@@ -14,38 +20,87 @@
     name: 'App',
     data() {
       return {
-        like: 0,
         data: [
           {
             title: "노랑",
             year: 2023,
             category: "액션, 드라마",
-            textRed: "color: red"
+            textRed: "color: red",
+            like: 0,
+            imgURL: "/assets/나트랑.jpg",
           },
           {
             title: "아쿠아맨과 로스트 킹덤",
             year: 2023,
             category: "액션, 판타지, 어드벤처",
+            like: 0,
+            imgURL: "/assets/멜버른.jpg",
           },
           {
             title: "3일의 휴가",
             year: 2023,
             category: "판타지, 드라마",
+            like: 0,
+            imgURL: "/assets/로마포지타노.jpg",
           },
         ]
       }
     },
     methods: {
-      increaseLike(){
-        this.like += 1;
+      increaseLike(i){
+        this.data[i].like += 1;
       }
     }
   }
 </script>
 
 <style>
-  .bg-yellow {
+  /* .bg-yellow {
       background: gold;
       padding: 10px;
-    }
+    } */
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body {
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  h1, h2, h3{
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+
+  button {
+    margin-right: 10px;
+  }
+
+  .item {
+    width: 100%;
+    border: 1px solid #ccc;
+    display: flex;
+    margin-bottom: 20px;
+    padding: 1rem;
+  }
+
+  .item figure {
+    width: 30%;
+    margin-right: 1rem;
+  }
+
+  .item img{
+    width: 100%;
+  }
+
+  .item .info {
+    width: 100%;
+  }
 </style>
