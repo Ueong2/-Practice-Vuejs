a9e42996c9a4cd55b98d2894b130b9627f1dd5bd
Ueong2 <kiko130@naver.com>
2025-03-10 13:29:28 +0900

    vue 학습1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>영화정보</h1>
+  <div>
+    <h3 class="bg-yellow" :style="textRed">{{ data[0].title }}</h3>
+    <p>개봉: {{ data[0].year }}</p>
+    <p>장르: {{ data[0].category }}</p>
+  </div>
+<!-- 
+  <p>{{ foods[0] }}</p>
+  <p>{{ foods[1] }}</p>
+  <p>{{ foods[2] }}</p>
+ -->
+  <!-- key 값이 반드시 필요. -->
+  <p v-for="(item, i) in foods" :key="i">{{ item }}</p>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        foods: ["김밥", "순대", "만두", "떡국"],
+        data: [
+          {
+            title: "노랑",
+            year: 2023,
+            category: "액션, 드라마",
+            textRed: "color: red"
+          },
+          {
+            title: "아쿠아맨과 로스트 킹덤",
+            year: 2023,
+            category: "액션, 판타지, 어드벤처",
+          },
+        ]
+      }
+    }
   }
-}
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .bg-yellow {
+      background: gold;
+      padding: 10px;
+    }
 </style>
