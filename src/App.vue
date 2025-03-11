f2febb0172470f195c4c5bbcf706a53f1decf8fc
Ueong2 <kiko130@naver.com>
2025-03-11 09:23:13 +0900

    외부 데이터 import

--- src/App.vue
+++ src/App.vue
@@ -25,43 +25,46 @@
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        isModal: false,
-        data: [
-          {
-            title: "노랑",
-            year: 2023,
-            category: "액션, 드라마",
-            textRed: "color: red",
-            like: 0,
-            imgURL: "/assets/나트랑.jpg",
-          },
-          {
-            title: "아쿠아맨과 로스트 킹덤",
-            year: 2023,
-            category: "액션, 판타지, 어드벤처",
-            like: 0,
-            imgURL: "/assets/멜버른.jpg",
-          },
-          {
-            title: "3일의 휴가",
-            year: 2023,
-            category: "판타지, 드라마",
-            like: 0,
-            imgURL: "/assets/로마포지타노.jpg",
-          },
-        ]
-      }
-    },
-    methods: {
-      increaseLike(i){
-        this.data[i].like += 1;
-      }
+import { food, city } from './assets/movies';
+console.log(food, city);
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isModal: false,
+      data: [
+        {
+          title: "노랑",
+          year: 2023,
+          category: "액션, 드라마",
+          textRed: "color: red",
+          like: 0,
+          imgURL: "/assets/나트랑.jpg",
+        },
+        {
+          title: "아쿠아맨과 로스트 킹덤",
+          year: 2023,
+          category: "액션, 판타지, 어드벤처",
+          like: 0,
+          imgURL: "/assets/멜버른.jpg",
+        },
+        {
+          title: "3일의 휴가",
+          year: 2023,
+          category: "판타지, 드라마",
+          like: 0,
+          imgURL: "/assets/로마포지타노.jpg",
+        },
+      ]
+    }
+  },
+  methods: {
+    increaseLike(i){
+      this.data[i].like += 1;
     }
   }
+}
 </script>
 
 <style>
