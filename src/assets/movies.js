624f7ea8db04f204af8d951284ea446a61c9b701
Ueong2 <kiko130@naver.com>
2025-03-11 09:28:44 +0900

    외부 데이터 import

--- src/assets/movies.js
+++ src/assets/movies.js
@@ -1,4 +1,28 @@
-let food = "mango";
-let city = "seoul";
+const data = [
+    {
+      title: "노랑",
+      year: 2023,
+      category: "액션, 드라마",
+      textRed: "color: red",
+      like: 0,
+      imgURL: "/assets/나트랑.jpg",
+    },
+    {
+      title: "아쿠아맨과 로스트 킹덤",
+      year: 2023,
+      category: "액션, 판타지, 어드벤처",
+      like: 0,
+      imgURL: "/assets/멜버른.jpg",
+    },
+    {
+      title: "3일의 휴가",
+      year: 2023,
+      category: "판타지, 드라마",
+      like: 0,
+      imgURL: "/assets/로마포지타노.jpg",
+    },
+  ]
 
-export { food, city }
+  // export 데이터가 1개인 경우
+  export default data;
+//   export { data }
